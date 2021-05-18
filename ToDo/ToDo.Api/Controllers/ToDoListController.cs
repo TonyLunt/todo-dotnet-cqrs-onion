f983a1867dcf52ba9943e2ce07d2385fc0a4ffcc
Tony Lunt <Tony.Lunt@insight.com>
2021-05-18 19:47:24 -0400

    more controller stuff

--- ToDo/ToDo.Api/Controllers/ToDoListController.cs
+++ ToDo/ToDo.Api/Controllers/ToDoListController.cs
@@ -5,6 +5,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ToDo.Application.Features.ToDoLists.Commands.CreateToDoListCommand;
+using ToDo.Application.Features.ToDoLists.Commands.DeleteToDoListCommand;
+using ToDo.Application.Features.ToDoLists.Commands.RenameToDoListCommand;
+using ToDo.Application.Features.ToDoLists.Commands.UpdateToDoListDescriptionCommand;
+using ToDo.Application.Features.ToDoLists.Queries.GetToDoListQuery;
 using ToDo.Application.Features.ToDoLists.Queries.ListToDoListQuery;
 using ToDo.Application.Features.ToDoLists.ViewModels;
 
@@ -24,5 +29,52 @@ namespace ToDo.Api.Controllers
             var response = await Mediator.Send(new ListToDoListQuery(), new System.Threading.CancellationToken());
             return Ok(response);
         }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<IEnumerable<ToDoListViewModel>>> Get([FromRoute]Guid id)
+        {
+            var response = await Mediator.Send(new GetToDoListQuery() { Id = id }, new System.Threading.CancellationToken());
+            return Ok(response);
+        }
+        [HttpGet]
+        [Route("{id}/Items")]
+        public async Task<ActionResult<IEnumerable<ToDoListViewModel>>> GetToDoItems([FromRoute] Guid id)
+        {
+            var response = await Mediator.Send(new GetToDoListQuery() { Id = id }, new System.Threading.CancellationToken());
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ToDoListViewModel>> Create(CreateToDoListCommand command)
+        {
+            var response = await Mediator.Send(command, new System.Threading.CancellationToken());
+            return Ok(response);
+        }
+
+        [HttpPut]
+        [Route("Name")]
+        public async Task<ActionResult<ToDoListViewModel>> Rename(RenameToDoListCommand command)
+        {
+            var response = await Mediator.Send(command, new System.Threading.CancellationToken());
+            return Ok(response);
+        }
+
+        [HttpPut]
+        [Route("Description")]
+        [HttpPut]
+        public async Task<ActionResult<ToDoListViewModel>> UpdateDescription(UpdateToDoListDescriptionCommand command)
+        {
+            var response = await Mediator.Send(command, new System.Threading.CancellationToken());
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<ToDoListViewModel>> DeleteToDoList(Guid id)
+        {
+            var response = await Mediator.Send(new DeleteToDoListCommand() { Id = id }, new System.Threading.CancellationToken());
+            return Ok(response);
+        }
+
     }
 }
