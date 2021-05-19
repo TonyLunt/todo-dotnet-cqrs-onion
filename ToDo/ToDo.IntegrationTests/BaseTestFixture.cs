ce9b48bb719a067e1229c138c73649322cefc57c
Tony Lunt <Tony.Lunt@insight.com>
2021-05-18 22:19:45 -0400

    starting on auth

---
+++ ToDo/ToDo.IntegrationTests/BaseTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.IntegrationTests
+{
+    pbulic class BaseTestFixture
+    {
+    }
+}
---
+++ ToDo/ToDo.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Infra.Data;
+
+namespace ToDo.IntegrationTests
+{
+    public class CustomWebApplicationFactory<TStartup>
+    : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<ToDoContext>));
+
+                services.Remove(descriptor);
+
+                //TODO: Replace this with actual data context eventually
+                services.AddDbContext<ToDoContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDatabase");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+            });
+        }
+    }
+}
